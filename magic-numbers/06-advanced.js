f6e043c07e80357877333ae31a417d234267bf43
simonsousa <simonsousa335@gmail.com>
2025-05-13 18:40:09 -0300

    terminando exercicio 06 de magic-numbers

--- magic-numbers/06-advanced.js
+++ magic-numbers/06-advanced.js
@@ -4,43 +4,73 @@
  * removendo todos os números mágicos e usando constantes significativas.
  */
 
+function listaPontuacaoDaSenha(){
+  return {
+    pontuacaoPadrao: 0,
+    minusculasOuMaiusculasA_Z : 2,
+    digitosDe0_a_9: 2,
+    caracteresEspeciais: 3
+  }
+}
+
 function validarSenha(senha) {
-  if (senha.length < 8) {
+  const COMPRIMENTO_MINIMO = 8;
+  if (senha.length < COMPRIMENTO_MINIMO) {
     return {
       valida: false,
       mensagem: "Senha muito curta",
     };
   }
 
-  let pontuacao = 0;
+  const {
+    pontuacaoPadrao, minusculasOuMaiusculasA_Z, digitosDe0_a_9, caracteresEspeciais
+  } = listaPontuacaoDaSenha();
+  
+  let pontuacao = pontuacaoPadrao;
 
-  if (senha.length >= 12) {
-    pontuacao += 2;
-  } else if (senha.length >= 10) {
-    pontuacao += 1;
-  }
+  const COMPRIMENTO_MAXIMO = 12;
+  const COMPRIMENTO_MEDIO = 10; 
+  
+  const PONTOS_COMPR_MAXIMO = 2;
+  const PONTOS_COMPR_MEDIO = 1;
 
-  if (/[A-Z]/.test(senha)) {
-    pontuacao += 2;
-  }
-  if (/[a-z]/.test(senha)) {
-    pontuacao += 2;
-  }
-  if (/[0-9]/.test(senha)) {
-    pontuacao += 2;
-  }
-  if (/[^A-Za-z0-9]/.test(senha)) {
-    pontuacao += 3;
+  if (senha.length >= COMPRIMENTO_MAXIMO) {
+    pontuacao += PONTOS_COMPR_MAXIMO;
+  } else if (senha.length >= COMPRIMENTO_MEDIO) {
+    pontuacao += PONTOS_COMPR_MEDIO;
   }
 
-  if (pontuacao < 5) {
+  const LETRAS_MAIUSC_A_Z = /[A-Z]/;
+  if (LETRAS_MAIUSC_A_Z.test(senha)) {
+    pontuacao += minusculasOuMaiusculasA_Z;
+  }
+
+  const LETRAS_MINUSC_A_Z = /[a-z]/;
+  if (LETRAS_MINUSC_A_Z.test(senha)) {
+    pontuacao += minusculasOuMaiusculasA_Z;
+  }
+
+  const DIGITOS_0_9 = /[0-9]/;
+  if (DIGITOS_0_9.test(senha)) {
+    pontuacao += digitosDe0_a_9;
+  }
+
+  const CARACTERES_ESPECIAIS = /[^A-Za-z0-9]/
+  if (CARACTERES_ESPECIAIS.test(senha)) {
+    pontuacao += caracteresEspeciais;
+  }
+
+  const PONTOS_SENHA_FRACA = 5;
+  if (pontuacao < PONTOS_SENHA_FRACA) {
     return { valida: false, mensagem: "Senha fraca" };
   }
-  if (pontuacao < 8) {
+
+  const PONTOS_SENHA_MEDIA = 8;
+  if (pontuacao < PONTOS_SENHA_MEDIA) {
     return { valida: true, mensagem: "Senha média" };
   }
 
-  return {
+  return { //Caso não caia nos dois primeiros ifs, a senha é forte
     valida: true,
     mensagem: "Senha forte",
   };
