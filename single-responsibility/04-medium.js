d1a2f1d5334f39edc948c007126f1f6f9458e6b2
simonsousa <simonsousa335@gmail.com>
2025-05-22 18:40:09 -0300

    refactor: refatorando algumas funções

--- single-responsibility/04-medium.js
+++ single-responsibility/04-medium.js
@@ -5,86 +5,111 @@
  */
 
   // Validação
-  function validarValor(dadosPagamento) {
-  if (!dadosPagamento.valor || dadosPagamento.valor <= 0) {
-      return { sucesso: false, mensagem: "Valor inválido" };
-    } 
-    return { sucesso: true, mensagem: ""};
+  // function validarValor(dadosPagamento) {
+  // if (!dadosPagamento.valor || dadosPagamento.valor <= 0) {
+  //     return { sucesso: false, mensagem: "Valor inválido" };
+  //   } 
+  //   return { sucesso: true, mensagem: ""};
+  // }
+
+  // function validarCartao(dadosPagamento){
+  //   if (
+  //     !dadosPagamento.cartao ||
+  //     !dadosPagamento.cartao.numero ||
+  //     !dadosPagamento.cartao.cvv
+  //   ) {
+  //     return { sucesso: false, mensagem: "Dados do cartão inválidos" };
+  //   }
+  //   return { sucesso: true, mensagem: ""};
+  // }
+
+  //  FORMATAÇÃO DO CARTÃO
+  // const numeroCartaoFormatado = dadosPagamento.cartao.numero.replace(
+  //   /\s+/g,
+  //   ""
+  // );
+
+  // TESTANDO SE O CARTÃO POSSUI 16 DIGITOS 
+  // if (!/^\d{16}$/.test(numeroCartaoFormatado)) { 
+  //   return { sucesso: false, mensagem: "Número de cartão inválido" };
+  // }
+
+function validarValor(dadosPagamento) {
+  if(!dadosPagamento.valor || dadosPagamento.valor <= 0) {
+    return { sucesso: false, mensagem: "Valor inválido" };
   }
+  return { sucesso: true, mensagem: ""};
+}
 
-  function validarCartao(dadosPagamento){
-    if (
-      !dadosPagamento.cartao ||
-      !dadosPagamento.cartao.numero ||
-      !dadosPagamento.cartao.cvv
-    ) {
-      return { sucesso: false, mensagem: "Dados do cartão inválidos" };
-    }
-    return { sucesso: true, mensagem: ""};
+function validarDadosCartao(dadosPagamento) {
+  if (
+    !dadosPagamento.cartao ||
+    !dadosPagamento.cartao.numero ||
+    !dadosPagamento.cartao.cvv
+  ) {
+    return { sucesso: false, mensagem: "Dados do cartão inválidos" };
   }
+  return { sucesso: true, mensagem: ""};
+}
 
-async function processarNoGateway(dadosPagamento, numeroCartao) {
-    // Processamento na gateway
-    try {
-      const resposta = await fetch("https://api.pagamento.com/processar", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          valor: valorTotal,
-          cartao: {
-            numero: numeroCartaoFormatado,
-            cvv: dadosPagamento.cartao.cvv,
-            validade: dadosPagamento.cartao.validade,
-          },
-        }),
-      });
+function formatarCartao(numeroCartao) {
+  return numeroCartao.replace(/\s+/g, "");//retira todos os espaços em branco do cartão
+}
 
-      const resultado = await resposta.json()
-      return resultado;
-    } catch (error) {
-      return undefined;
-    }
+function testarCartaoCom16Digitos(numeroCartaoFormatado) {
+  if (!/^\d{16}$/.test(numeroCartaoFormatado)) {//REGEX : Se o cartão não possuir 16 dígitos
+    return { sucesso: false, mensagem: "Número de cartão inválido"};
+  }
+  return { sucesso: true, mensagem: ""};
+}
+
+function listarTaxas() {
+  return {
+    TAXA_PADRAO: 0,
+    TAXA_1_POR_CENTO: 0.01,
+    TAXA_3_POR_CENTO: 0.03
+  }
 }
 
 async function processarPagamento(dadosPagamento) {
-  const validacaoValor = validarValor(dadosPagamento);
-  if(!validacaoValor.sucesso){
-    return validacaoValor;
-  }
+  // Validação
+  const valorPagamento = validarValor(dadosPagamento);
 
-  const validacaoCartao = validarCartao(dadosPagamento)
-  if(!validacaoCartao.sucesso) {
-    return validacaoCartao;
-  }
-
-  return { sucesso: true, mensagem: ""} 
+  validarDadosCartao(valorPagamento);
 
   // Formatação do cartão
-  const numeroCartaoFormatado = dadosPagamento.cartao.numero.replace(
-    /\s+/g,
-    ""
-  );
+  const numeroCartaoFormatado = formatarCartao(dadosPagamento.cartao.numero)
   
-  if (!/^\d{16}$/.test(numeroCartaoFormatado)) {
-    return { sucesso: false, mensagem: "Número de cartão inválido" };
-  }
+  testarCartaoCom16Digitos(numeroCartaoFormatado);
 
   // Cálculo de taxa
-  let taxa = 0;
+  const { TAXA_PADRAO, TAXA_1_POR_CENTO, TAXA_3_POR_CENTO } = listarTaxas();
+
+  let taxa = TAXA_PADRAO;
   if (dadosPagamento.metodoPagamento === "credito") {
-    taxa = dadosPagamento.valor * 0.03;
+    taxa = dadosPagamento.valor * TAXA_3_POR_CENTO;
   } else if (dadosPagamento.metodoPagamento === "debito") {
-    taxa = dadosPagamento.valor * 0.01;
+    taxa = dadosPagamento.valor * TAXA_1_POR_CENTO;
   }
 
   const valorTotal = dadosPagamento.valor + taxa;
 
+  // Processamento na gateway
+  try {
+    const resposta = await fetch("https://api.pagamento.com/processar", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        valor: valorTotal,
+        cartao: {
+          numero: numeroCartaoFormatado,
+          cvv: dadosPagamento.cartao.cvv,
+          validade: dadosPagamento.cartao.validade,
+        },
+      }),
+    });
 
-
-    const resultado = await processarNoGateway(dadosPagamento, numeroCartaoFormatado);
-    if(!resultado){
-      return { sucesso: false, mensagem: "Erro ao processar a compra"}
-    }
+    const resultado = await resposta.json();
 
     // Registro do pagamento
     console.log(
